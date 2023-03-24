9379fedb1d98dbbb3ae33ef60769c0abfbae890f
LukeMocroski <devmocroski@gmail.com>
2023-03-24 10:03:48 -0300

    Components Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [ServerComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
